c0d5de106837aac40473455d9e911222396b401d
Dagim Demissew <dagim99demis@gmail.com>
2024-08-15 18:14:05 +0300

    build the home page, menu item and directory components and finished styling.

---
+++ src/Pages/homePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Directory from "../../components/directory/Directory";
+import MenuItem from "../../components/menuItem/MenuItem";
+import "./homepage.scss";
+
+const HomePage = () => {
+  return (
+    <div className="homePage">
+      <Directory />
+    </div>
+  );
+};
+
+export default HomePage;
