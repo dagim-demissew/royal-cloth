ccd4e231f09ae25a4356c1c63f7c040abdab7848
Dagim Demissew <dagim99demis@gmail.com>
2024-08-17 15:03:45 +0300

    created routing, added shop page and collection preview component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/homePage/HomePage";
+import "./App.css";
+import ShopPage from "./Pages/shopPage/Shop";
 
 function App() {
   return (
     <>
-      <HomePage />
+      <Routes>
+        <Route exact path="/" Component={HomePage} />
+        <Route exact path="/shop" Component={ShopPage} />
+      </Routes>
     </>
   );
 }
